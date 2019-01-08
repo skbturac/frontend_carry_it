385ca313dcf57c2d7e78295b2bdca5b01492dc0e
github username <github email address>
2019-01-08 13:39:07 -0500

    FrontEnd In Progress

--- src/containers/Homepage.js
+++ src/containers/Homepage.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import 'semantic-ui-css/semantic.min.css'
+import MenuBar from '../components/MenuBar'
+import {
+  Menu,
+  Segment,
+  Header,
+  Container,
+  Button,
+  Icon
+  } from 'semantic-ui-react'
 
 class Homepage extends Component {
+
   render() {
     return (
       <div className="homepage-container">
-        "Homepage"
+        <div className="header-container">
+          <Header size='huge'>Carry It</Header>
+        </div>
+        <div className="Menu-bar">
+          <Menu />
+        </div>
+        <div className="post-container">
+          <Header size='medium'>posts</Header>
+        </div>
+
       </div>
     );
   }
--- src/containers/PostContainer.js
+++ src/containers/PostContainer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class PostContainer extends Component {
   render() {
     return (
-      <div className="post-container">
+      <div className="posts-container">
         "PostContainer"
       </div>
     );
