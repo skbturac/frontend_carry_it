6cfd3ecfbcf0e318aa1b96b23580546c074fd793
github username <github email address>
2019-01-09 14:55:17 -0500

    SignIn SignUp page Setup in Progress

--- src/components/UserLoginForm.js
+++ src/components/UserLoginForm.js
@@ -3,12 +3,55 @@ import { Form, Button } from 'semantic-ui-react'
 
 
 class UserLoginForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      userNameInput: '',
+      passwordInput: ''
+    }
+  }
+
+  handleInputChange = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.userNameInput);
+    this.setState({
+      [event.target.name]:event.target.value,
+    })
+  }
+
+  handleSubmit = () => {
+    // const body = {
+    //
+    // }
+    // fetch('http:localhost:3000/api/v1/users', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     Accept: 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     user: {
+    //       username: 'guy',
+    //       password: 'hi',
+    //       bio: 'King of Flavortown, USA',
+    //       avatar: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Guy_Fieri_at_Guantanamo_2.jpg'
+    //     }
+    //   })
+    // })
+    // .then(response => response.json())
+    // .then(console.log)
+  }
+
   render() {
+    console.log(this.state.userNameInput);
+    console.log(this.state.passwordInput);
     return (
       <div className="user-login-form">
-        <Form>
-          <Form.Input icon='user' iconPosition='left' label='Username' placeholder='Username' />
-          <Form.Input icon='lock' iconPosition='left' label='Password' type='password' />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Input icon='user' iconPosition='left' label='Username' placeholder='Username' value={this.state.userNameInput} onChange={this.handleInputChange}
+          name='userNameInput'/>
+          <Form.Input icon='lock' iconPosition='left' label='Password' type='password' value={this.state.passwordInput} onChange={this.handleInputChange}
+          name='passwordInput'/>
 
           <Button content='Login' primary />
         </Form>
--- src/components/UserSignUpForm.js
+++ src/components/UserSignUpForm.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
+import { Button, Form } from 'semantic-ui-react'
 
 
 class UserSignUpForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      firstNameInput: '',
+      lastNameInput: '',
+      userNameInput: '',
+      passwordInput: '',
+    }
+  }
+
+  handleInputChange = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.userNameInput);
+    this.setState({
+      [event.target.name]:event.target.value,
+    })
+  }
+
+  handleSubmit = () => {
+    fetch('http://localhost:4000/api/v1/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          first_name: this.state.firstNameInput,
+          last_name: this.state.lastNameInput,
+          username: this.state.userNameInput,
+          password: this.state.passwordInput,
+          bio: 'King of Flavortown, USA',
+          avatar: 'https://upload.wikimedia.org/wikipedia/commons/9/9a/Guy_Fieri_at_Guantanamo_2.jpg'
+        }
+      })
+    })
+    // .then(response => response.json())
+    // .then(console.log)
+  }
+
   render() {
+    console.log(this.state.userNameInput);
+    console.log(this.state.passwordInput);
     return (
       <div className="user-signup-form">
-        <Button content='Sign up' icon='signup' size='big' />
+      <Form onSubmit={this.handleSubmit}>
+        <Form.Input iconPosition='left' label='First Name' placeholder='First Name' value={this.state.firstNameInput} onChange={this.handleInputChange}
+        name='firstNameInput' />
+        <Form.Input iconPosition='left' label='Last Name' placeholder='Last Name' value={this.state.lastNameInput} onChange={this.handleInputChange}
+        name='lastNameInput'  />
+        <Form.Input iconPosition='left' label='Username' placeholder='Username' value={this.state.userNameInput} onChange={this.handleInputChange}
+        name='userNameInput' />
+        <Form.Input icon='lock' iconPosition='left' label='Password' type='password' value={this.state.passwordInput} onChange={this.handleInputChange}
+        name='passwordInput' />
+
+        <Button content='Sign up' icon='signup' size='medium' primary/>
+        </Form>
       </div>
     );
   }
